989c3200bbe2ffd3e6b845fec21defa37756bfb6
void4 <void4@users.noreply.github.com>
2021-06-02 09:25:24 +0200

    Update pairclient.py to Python3

--- source/pyzmq/patterns/code/pairclient.py
+++ source/pyzmq/patterns/code/pairclient.py
@@ -9,8 +9,8 @@ socket = context.socket(zmq.PAIR)
 socket.connect("tcp://localhost:%s" % port)
 
 while True:
-    msg = socket.recv()
-    print msg
-    socket.send("client message to server1")
-    socket.send("client message to server2")
+    msg = socket.recv_string()
+    print(msg)
+    socket.send_string("client message to server1")
+    socket.send_string("client message to server2")
     time.sleep(1)
